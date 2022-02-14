ad99bd337b37769fb06e5c953c98687ec609926c
Leevi Kokkinen <leevi.kokkinen@gmail.com>
2022-02-14 21:06:43 +0200

    commit

--- index.js
+++ index.js
@@ -5,18 +5,6 @@ const app = express()
 
 app.use(express.json())
 
-morgan.token('content', function (req, res) { return JSON.stringify(req.body) })
-
-morgan(function (tokens, req, res) {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms',
-      tokens['content'](req,res)
-    ].join(' ')
-  })
 app.use(morgan('tiny'))
 
 let persons = [

4aa96a8d1a26c451ef49f5c2d90558c6cccc32b7
Leevi Kokkinen <leevi.kokkinen@gmail.com>
2022-02-14 20:32:05 +0200

    tehtävään 3.7

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+const { response } = require('express')
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+
+morgan.token('content', function (req, res) { return JSON.stringify(req.body) })
+
+morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      tokens['content'](req,res)
+    ].join(' ')
+  })
+app.use(morgan('tiny'))
 
 let persons = [
     {
@@ -34,6 +50,51 @@ app.get('/info', (req, res) => {
     res.send(`<p>Phonebook has info for ${persons.length} people
     </p>
     <p>${new Date()}</p>`)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        res.send(`<p>${person.name}</p>
+        <p>${person.number}</p>
+        `)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    if (!body.name || !body.number) {
+        return res.status(400).json({
+            error: "name or number missing"
+        })
+    }
+
+    if (persons.find(person => (person.name === body.name))) {
+        return res.status(409).json({
+            error: "name must be unique"
+        })
+    }
+
+
+    const person = {
+        id: Math.round(Math.random()*1000),
+        name: body.name,
+        number: body.number
+    }
+    persons = persons.concat(person)
+    res.json(person)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
 })
 
 const PORT = 3001
